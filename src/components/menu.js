e6e0aae2fe432e966ab765c9806d32fcaf52ee60
Aadhitya A <aadhitya864@gmail.com>
2023-11-25 19:49:50 +0530

    Fix resume link
    Signed-off-by: Aadhitya A <aadhitya864@gmail.com>

--- src/components/menu.js
+++ src/components/menu.js
@@ -266,7 +266,7 @@ const Menu = () => {
               </ol>
             )}
 
-            <a href="/resume.pdf" className="resume-link">
+            <a href="https://tinyurl.com/resume-alyx" className="resume-link">
               Resume
             </a>
           </nav>
